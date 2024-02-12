bf061add6cefcf3303b1050ae5c079dd7810d48b
xvillarroel <xaviervillarroelparra@gmail.com>
2024-02-12 13:21:45 -0300

    Packages installed

--- index.mjs
+++ index.mjs
@@ -1,2 +1,6 @@
 // https://fq6usk6c7kbsrdwstmklvki7x40ececm.lambda-url.us-east-1.on.aws/
 
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { JWT } from 'google-auth-library';
+import fetch from 'node-fetch';
+
