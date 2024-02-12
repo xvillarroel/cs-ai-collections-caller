25c376c868feef8773ae17a3be26eb4b615e4561
xvillarroel <xaviervillarroelparra@gmail.com>
2024-02-12 18:30:53 -0300

    Working.

--- index.mjs
+++ index.mjs
@@ -12,6 +12,7 @@ const globals = {
     OPENAI_APIKEY:  'Bearer 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',
     ZDDOMAIN:       'central-supportdesk.zendesk.com',
     ZDAUTH:         'Basic Y2FybG9zLmVuY2FsYWRhQHRyaWxvZ3kuY29tL3Rva2VuOmhnQTltaFFvQjlWV1dnaGV6UjZFeEdDZFlPTFZMRFp3dUlmU1RTbGE=',
+    SHEETS_LINK:         '13rBFlGSpmXah2pzvUjYDm6xsv5tDhGFBu1q2ImvQQVk',
     SERVICEACCOUNTAUTH: new JWT({
         email:  'javascript-writer@groupsproject-370909.iam.gserviceaccount.com',
         key:    '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCtAUZp72L3zq+6\nGdBUrgGqs7kbUtWx0mKHfdMiliRsLe6wj0UYdhSqU/Nd505ic0EGRwrhxL3rq4FV\nIQI5creAGfNWzOZKtAPMV4YoSqN8HgfvF/DrLgGAwScn56dVituCOxv4j9pXQIIo\nMiIm9O13mWlEBEFjw+jXP/Z+6M7Rgk25lDZR0/1A/KX5eG/XJ0fmcC+TSM0NpS6V\nKrbqHweFfgLDTbUqsf8TUOqLFJ0Qaj7VBuGjd9yp9+icZ68giksMuyFdqlo4MKD6\ns2WW3YR2NeSbQ/+D+6IGWyts+9NA9OVDazaT1nJ6+KYO+Oq6c1l8lxS9ur01JrSU\nPIP1PLTjAgMBAAECggEAOG8MCw5dmDxBslEtVhIU1RwfK7yPnJvmLBBtSQD1DJzH\nGa0CewI5p34PCvii5xZ1hZizAgZtdWzSmXRVB2xWU2EjsZLRJFkoTAXY61e9kIUz\nTVjf67dsIhXfsfKs8QiEpiyl6STzsjaGvpnr7g1DURon7ln9ApAraduhirBilpCD\ne6HpFv81gRT+2ujzFvWWW4NE0E2uy8LxnOxSyFGOpK4PUWPpoJuLf52MCwOae+oB\nRXKpxjMjM9kBJ4Rex9ugR6ETwkf1JDQfa/uM/cOlEQjqS9rp20jbZCWPtYfSpLt0\n+nr+TfM+nGYYu0zWRLH6mTHVnHGItFcjUAQTIZdk0QKBgQDWYglr4IIAZh6BYe/z\nbXI+mQN9UbkTzQ691abyjY5pUEu0lKd/HrYwS0Phzj+5lhaUtrEvbN0HclaTCEd7\n10v0m30jiwJPQDmu9RXj6Tl3qKb2rxnHLviX9ypJ7mkDHO7KTRDahVvm6jZmcHnR\nzNA/9mYLpiVNpGQakoyVVX3vrQKBgQDOluu1C2j5PCuGkmkEEOSb4uZt9MvTGdDi\ns/7fcPEyPS/1Vbh/IrKwBBSnfpcUfCpD1dC8YbQhjsHewdPqnpiOHFj9hOsoQeH4\nRYaZTRFAW95a6LnBRmTXqCWyru8Q2Llr3Zz0agtc/L1+LvF2jcltjNgsAfed0Gu3\nNiswM+SIzwKBgEH4QxvuJzMGOabow1TuPfSjU16R8lj0he/GuivzgXpI2jMEd5J3\nmeq8jnQC6rsqQ2KZ4WZNoqpy9c8jAhRKyTXJTzXLxfcrNVTwWD8c+rEmtdI9Sbpw\natEgnuPHOItbsOOR2XjVBtXFBt55CBOWahL0uKwnAV2mE6PVqusdNra1AoGARbJI\n5xVoXt1b2dS/NS310lmkX+g8c4W8IR+UlxFlbguSiHRZABtWqWdXCIL+uVyCbcxO\n1Z8oxEGDSoGd2wOSeC88HpufMj+32qiqFkIX1dyokYb+VCRJlTAXN8coxEg5lhh4\nAUVdfAuQamev8s027YycyYwIW+eaz36o52Q6b6UCgYAUs79KmYnzSWBI4NGueCGB\n6isZWyB/CXm7FO19YNLeUZSaeCtV6MpE3bnuu/lD0RTZeh3kO9iCU/hSBe2NLywt\nR5sECyYkR5WA4on/XUk5dlPu1XGtrdH9EBg6idDTdbSoE5r1PAQ3uztYAVecztOW\nEovCHd0/5Gw1Cw5aanfUqw==\n-----END PRIVATE KEY-----\n',
@@ -38,11 +39,11 @@ const convertToMatrix = (row) => {
             return array; 
         }  
     );
-}
+};
 
 const filterMatrix = (row, shift) => {
     return row.filter(row => row[8] === shift);
-}
+};
 
 const makeCall = async (phoneNumber) => {
     const url = `https://voiceflow-twilio-collections.replit.app/ivr/call?phone=${phoneNumber}`;
@@ -65,9 +66,9 @@ const makeCall = async (phoneNumber) => {
       console.error('Call error:', error);
       throw error;
     }
-  };
+};
 
-export const handler = async (event, context) => {
+export const handler = async (event, context) => {  
 
     console.log (`****************\n Starting Function - V1 (PROD VERSION) \n****************`)
     let response; 
@@ -89,7 +90,7 @@ export const handler = async (event, context) => {
         }
 
     const shift2Call = body.shift;
-    const doc = new GoogleSpreadsheet('13rBFlGSpmXah2pzvUjYDm6xsv5tDhGFBu1q2ImvQQVk', globals.SERVICEACCOUNTAUTH);
+    const doc = new GoogleSpreadsheet(globals.SHEETS_LINK, globals.SERVICEACCOUNTAUTH);
     await doc.loadInfo();
     let tabName = 'Raw';
     console.log(`Title of the doc: ${doc.title}`);
@@ -104,20 +105,42 @@ export const handler = async (event, context) => {
 
     await activeSheet.loadCells('A1:Z');
     let rows = await activeSheet.getRows();
-    let matrix = convertToMatrix(rows); // In this moment, I have a matrix that starts with A2 and ends with P-whatever.
+    let matrix = convertToMatrix(rows); 
 
     // let rowsObject = divideMatrix(matrix);
     
     matrix = filterMatrix(matrix, shift2Call)
 
-    console.log(matrix);
+    if (!matrix.length){
+        response =  await assembleResponse(400,'No one to call in this shift'); 
+        console.log(JSON.stringify(response,null,2));
+        return response;
+    } 
 
     for (let i = 0; i < matrix.length; i++){
+
         let phone2Call = matrix[i][3]
                         .replace(/ /g,'')
                         .replace(/\+/g,'');
-        response = await makeCall(phone2Call)
-        console.log(`Calling customer ${matrix[i][2]} at ${phone2Call} (Shift ${matrix[i][8]}). Status: ${response.status}`)
+
+        let alreadyCalled = (matrix[i][9] === 'FALSE') ? false : true;
+        if (!alreadyCalled){ 
+
+            response = await makeCall(phone2Call)
+            console.log(`Calling customer ${matrix[i][2]} at ${phone2Call} (Shift ${matrix[i][8]}). Status: ${response.status}`)
+            
+            if (response.status.search('Call initiated to') >= 0) {
+
+                rows[i].set('Called', true);
+                rows[i].set('AP Contact Number', `'+${phone2Call.toString()}`);
+                await rows[i].save();
+
+            }
+
+        } else {
+            console.log(`${phone2Call} (${matrix[i][2]}) was already called.`)
+        }
+
     }
 
     
